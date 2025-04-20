c8c13d8b9dca77f7669b53b2bd302e07593c5c37
Andrew Cudworth <acudworth3@gmail.com>
2025-04-19 21:09:23 -0400

    finsh up component notes

--- src/components/scenario.tsx
+++ src/components/scenario.tsx
@@ -12,16 +12,21 @@ type Props = {
 // These are some brief notes on what I expect this component to do 
 // - It is a modal that uses the dialog html element 
 // - within the element is  heading and a button 
-// - The close button has an onClose handler that closes the modal by changing the X value when clicked or escape is used is clicked. It also uses a data test id to support rendering .
+
+
+// - The close button has an onClose handler that closes the modal I did not
+// finish scaffolding this out, but would have done the following
+// - add a useState
+// - create a ternary based on the state value to conditionally render the component
 export const Scenario = (props: Props) => {
   // there is a better way
   const dataTestId = props.dataTestId
   return (
     <>
       <dialog role="dialog" data-test-id="good-dialog">
-        <h2 role="heading" data-test-id="good-heading">close</h2>
+        <h2 role="heading" data-test-id="good-heading">Header</h2>
         <button role="button" data-test-id={dataTestId}>close</button>
-
+        Vestibulum sit amet nibh turpis.
       </dialog>
     </>
 
