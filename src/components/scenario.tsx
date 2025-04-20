0b1c40f61482daca7cbd06b67082b7ceefeae2d7
Andrew Cudworth <acudworth3@gmail.com>
2025-04-19 21:06:39 -0400

    finish out scaffolding

--- src/components/scenario.tsx
+++ src/components/scenario.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 
 type Props = {
   value: string
@@ -7,11 +8,22 @@ type Props = {
 
 }
 
+// I am fighting with typescript to get these tests running . 
+// These are some brief notes on what I expect this component to do 
+// - It is a modal that uses the dialog html element 
+// - within the element is  heading and a button 
+// - The close button has an onClose handler that closes the modal by changing the X value when clicked or escape is used is clicked. It also uses a data test id to support rendering .
 export const Scenario = (props: Props) => {
-  console.info(props)
+  // there is a better way
+  const dataTestId = props.dataTestId
   return (
     <>
-      <div>Scenario </div>
+      <dialog role="dialog" data-test-id="good-dialog">
+        <h2 role="heading" data-test-id="good-heading">close</h2>
+        <button role="button" data-test-id={dataTestId}>close</button>
+
+      </dialog>
     </>
+
   )
 }
--- src/tests/scenario.test.ts
+++ src/tests/scenario.test.ts
@@ -4,6 +4,7 @@ import { render, screen } from '@testing-library/react'
 import { Scenario } from '../components/scenario';
 
 
+
 describe('Modal', () => {
   const mockClose = vi.fn();
 
